c0ae7753eaef8f5111a04d38eb8d2616be73c152
Tony <nya.tony2010@gmail.com>
2023-08-04 15:15:09 +0100

    Lesson 10 - work with git

--- src/components/lessons/07/lesson-07.tsx
+++ src/components/lessons/07/lesson-07.tsx
@@ -99,42 +99,42 @@ export const Lesson07 = () => {
                     color={getColor(1)}
                     icon="arrow"
                     className={styles.icon}
-                    iconDirection="right"
+                    iconDirection="down"
                 />
                 <Box
                     outlined
                     color={getColor(2)}
                     icon="arrow"
                     className={styles.icon}
-                    iconDirection="up"
+                    iconDirection="down"
                 />
                 <Box
                     outlined
                     color={getColor(3)}
                     icon="arrow"
                     className={styles.icon}
-                    iconDirection="down"
+                    iconDirection="up"
                 />
                 <Box
                     outlined
                     color={getColor(4)}
                     icon="arrow"
                     className={styles.icon}
-                    iconDirection="right"
+                    iconDirection="down"
                 />
                 <Box
                     outlined
                     color={getColor(5)}
                     icon="arrow"
                     className={styles.icon}
-                    iconDirection="left"
+                    iconDirection="right"
                 />
                 <Box
                     outlined
                     color={getColor(6)}
                     icon="arrow"
                     className={styles.icon}
-                    iconDirection="right"
+                    iconDirection="up"
                 />
                 <Box
                     outlined
